40ecbec547ff46f994ce5844a1a6066430963446
VladTrokhymchuk <assistantl7497@gmail.com>
2022-08-24 21:54:36 +0300

    head nav

--- header.php
+++ header.php
@@ -16,45 +16,51 @@
 <body <?php body_class( $class ) ?>>
 
     <header class="header-section">
-        <div class="container">
-
-            <div class="header__top">
-                <div class='navbar'>
-                    <?php wp_nav_menu(array(
+        <div class="container header__top">
+            <div class='navbar'>
+                <?php wp_nav_menu(array(
                         'menu' => 'Fullpage menu',
                         'container'       => 'div',
                         'container_class' => 'menu',
                         'theme_location' => 'primary',
                         'menu_class' => 'menu-list'));
                     ?>
-                </div>
-
-                <div class="header__toggler">
-                    <label class="toggler-wrapper style-1">
-                        <input type="checkbox">
-                        <div class="toggler-slider">
-                            <div class="toggler-knob"></div>
-                        </div>
-                    </label>
-                    <span>dark</span>
-                </div>
-
-                <?php get_template_part('./template-parts/block/block-social'); ?>
             </div>
+            <div class="header__toggler">
+                <label class="toggler-wrapper style-1">
+                    <input type="checkbox">
+                    <div class="toggler-slider">
+                        <div class="toggler-knob"></div>
+                    </div>
+                </label>
+                <span>dark</span>
+            </div>
+            <?php get_template_part('./template-parts/block/block-social'); ?>
+        </div>
 
-            <div class="header__bott">
+        <div class="container header__bott">
+            <a class="header__bott__logo" href='<?= get_home_url() ?>'>
                 <?php
                     $img_logo = get_field('img_logo', 'options');
-                    if ($img_logo):
-                    $link = get_field('logo_link', 'options');
+                    $svg_markup = file_get_contents( get_attached_file( $img_logo['ID'] ) );
+                    echo $svg_markup;
                 ?>
-
-                <a class="header__logo" href='<?= esc_url($link['url']); ?>' title="<?=esc_html( $link['title'] ); ?>">
+            </a>
+            <div class='header__bott__navbar'>
+                <?php wp_nav_menu(array(
+                    'menu' => 'Second menu',
+                    'container'       => 'div',
+                    'container_class' => 'menu',
+                    'theme_location' => 'primary',
+                    'menu_class' => 'header__bott__navbar__list'));
+                ?>
+            </div>
+            <div class="btn-search">
+                <?php if ( get_field('icon_search', 'options') ) : $icon_search = get_field('icon_search', 'options'); ?>
                     <?php
-                        $svg_markup = file_get_contents( get_attached_file( $img_logo['ID'] ) );
-                        echo $svg_markup;
+                        $svg_search = file_get_contents( get_attached_file( $icon_search['ID'] ) );
+                        echo $svg_search;
                     ?>
-                </a>
                 <?php endif; ?>
             </div>
         </div>
